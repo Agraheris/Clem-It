8ebe071ae592c067b91107c7051f7bacbbbb7627
clement Vigouroux <clement.vigouroux@gmail.com>
2025-03-25 13:37:18 +0100

    add logo in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,17 @@
 import { FaLinkedin, FaInstagram, FaTiktok } from "react-icons/fa";
+import Logo from "../../public/assets/Logo.webp"
 
 function Footer() {
   return (
-    <footer className="bg-gray-900 text-white py-8 mt-16">
-      <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-6">
-        <p className="text-center text-sm md:text-left">
+    <footer className="bg-gray-900 text-white mt-16">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-6 text-2xl">
+        <p className="text-center text-2 md:text-left">
           © {new Date().getFullYear()} Vigouroux Clément — Tous droits réservés.
           <br/>
           Siret 94111475300010
         </p>
-        <div className="flex gap-6 text-2xl">
+        <img src={Logo} className="w-32"/>
+        <div className="flex gap-6 text-4xl">
           <a
             href="https://www.linkedin.com/in/cl%C3%A9ment-vigouroux-41b23a151/"
             target="_blank"
